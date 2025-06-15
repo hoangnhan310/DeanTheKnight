23e11bf1af27b22de82ed9f3be49c94e4dcc6ceb
Huynh Gia Bao <baohgce180399@fpt.edu.vn>
2025-06-16 06:44:37 +0700

    Adding EnemyBehaviour to control the enemy

--- Assets/Scripts/Utilities/Background/BackgroundController.cs
+++ Assets/Scripts/Utilities/Background/BackgroundController.cs
@@ -10,11 +10,17 @@ public class BackgroundController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        startPositionX = transform.position.x;
+        startPositionX = transform.position.x - 10;
     }
 
     // Update is called once per frame
     void FixedUpdate()
+    {
+        CameraTransform();
+    }
+
+    //Transform the background based on the camera's position
+    public void CameraTransform()
     {
         float distance = came.transform.position.x * parallaxSpeed; //0 = move with camera || 1 = won't move || 0.5 = half
 
