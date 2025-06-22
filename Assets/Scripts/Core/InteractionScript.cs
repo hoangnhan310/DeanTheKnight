891469cf9d6821c4c9d82d5a17d4acc728a4c35a
nhatduy8866 <nhatduy8866@gmail.com>
2025-06-23 00:30:22 +0700

    Change monster in map 7 pixel per unit, add chest, add script and animation for enemies' attack
    Change monster in map 7 pixel per unit, add chest, add script and animation for enemies' attack and opening chest

---
+++ Assets/Scripts/Core/InteractionScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class InteractionScript : MonoBehaviour
+{
+    public string interactionKey = "e";      // Phím bấm
+    public string triggerName = "Activate";  // Tên trigger trong Animator
+
+    private bool playerInRange = false;
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (playerInRange && Input.GetKeyDown(interactionKey))
+        {
+            animator.SetTrigger(triggerName);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Enemies/Map7/EnemyAttack.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class EnemyAttackTrigger : MonoBehaviour
+{
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            animator.SetBool("Attacking", true);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            animator.SetBool("Attacking", false);
+        }
+    }
+}
