3387edd9c13fb1297c90d45cb74eccbed814908a
nhatduy8866 <nhatduy8866@gmail.com>
2025-07-19 14:39:59 +0700

    Add HUD and Defeated Canvas

---
+++ Assets/Scripts/UI/DefeatedUI.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DefeatedUI : MonoBehaviour
+{
+    private void OnEnableDefeated()
+    {
+        Time.timeScale = 0f;
+    }
+
+    private void OnDisableDefeated()
+    {
+        Time.timeScale = 1f;
+    }
+
+    private void UpdateDefeated()
+    {
+        if (gameObject.activeSelf && Input.GetKeyDown(KeyCode.Escape))
+        {
+            return;
+        }
+    }
+
+    public void ToMenuDefeated()
+    {
+        string currentSceneName = SceneManager.GetActiveScene().name;
+        PlayerPrefs.SetString("LastScene", currentSceneName);
+        PlayerPrefs.Save();
+
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void RestartDefeated()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
