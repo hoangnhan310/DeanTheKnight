44cac023c07e0745775102204910542b3244c44c
hoangnhan310 <nhanlekame@gmail.com>
2025-07-19 11:45:32 +0700

    Redesign aniamtion AI for map4,map5

---
+++ Assets/Scripts/Enemies/map4/EnemyAttackHitbox.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class EnemyAttackHitbox : MonoBehaviour
+{
+    [SerializeField] private float damage = 10f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            // Gây sát thương nếu player có component TakeDamage
+            PlayerHealth playerHealth = collision.GetComponent<PlayerHealth>();
+            if (playerHealth != null)
+            {
+                playerHealth.TakeDamage(damage);
+                Debug.Log($"Enemy hit player for {damage} damage");
+            }
+        }
+    }
+}
